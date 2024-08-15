2ab5f567e88ebe2f1a89d99f564aff770d5c9ec1
AhnJaeHyeok <149498624+AhnJaeHyeok@users.noreply.github.com>
2024-08-15 21:02:41 +0900

    firebase 적용완료

--- src/pages/Main.svelte
+++ src/pages/Main.svelte
@@ -1,6 +1,41 @@
 <script>
+  import { onMount } from "svelte";
+  import Footer from "../components/Footer.svelte";
+  import { getDatabase, ref, onValue } from "firebase/database";
+
+  //여기 위에 있는 자바스크립트 파일은
+  // 이 화면이 처음 렌더링 될때 처음 뜰 떄 한번만 뜨게 된다(한번실행)
+  //그리고 나서 데이터를 가져오지 않음
+  //화면이 보일때마다 실행되게 하고싶으면 onMount를 쓰면된다
   let hour = new Date().getHours();
   let min = new Date().getMinutes();
+
+  $: items = []; // 5.이 문법을 사용할시 svelte가 알아서 업데이트
+  //let 을 써도 값 자체가 바뀌기 때문에 let도 가능
+  const calcTime = (timestamp) => {
+    //한국시간 UTC +9시간
+    const curTime = new Date().getTime() - 9 * 60 * 60 * 1000;
+    const time = new Date(curTime - timestamp);
+    const hour = time.getHours();
+    const minute = time.getMinutes();
+    const second = time.getSeconds();
+
+    if (hour > 0) return `${hour}시간 전`;
+    else if (minute > 0) return `${minute}분 전`;
+    else if (second >= 0) return `${second}초 전`;
+    else return "방금 전";
+  };
+
+  const db = getDatabase();
+  const itemsRef = ref(db, "items/"); //1.이 값이 바뀔때마다
+  onMount(() => {
+    //onMount 추가 왜? 글쓰기 갔다오면 이미지 사라짐 현상때문에
+    onValue(itemsRef, (snapshot) => {
+      //2.스냅샷이 새롭게 내려온다
+      const data = snapshot.val(); //3.바뀐 스냅샷은 이거야 너 화면 업데이트해
+      items = Object.values(data).reverse(); // 4.이 데이터를 가지고 items를 업데이트 하고
+    });
+  });
 </script>
 
 <header>
@@ -29,42 +64,25 @@
 </header>
 
 <main>
+  {#each items as item}
+    <div class="item-list">
+      <div class="item-list__img">
+        <img alt={item.title} src={item.imgUrl} />
+      </div>
+      <div class="item-list__info">
+        <div class="item-list__info-title">{item.title}</div>
+        <div class="item-list__info-meta">
+          {item.place}
+          {calcTime(item.insertAt)}
+        </div>
+        <div class="item-list__info-price">{item.price}</div>
+        <div>{item.description}</div>
+      </div>
+    </div>
+  {/each}
   <a class="write-btn" href="#/write">+ 글쓰기</a>
 </main>
 
-<footer>
-  <div class="footer-block">
-    <div class="footer-icons">
-      <div class="footer-icons__img">
-        <img src="assets/home.svg" alt="" />
-      </div>
-      <div class="footer-icons__desc">홈</div>
-    </div>
-    <div class="footer-icons">
-      <div class="footer-icons__img">
-        <img src="assets/doc.svg" alt="" />
-      </div>
-      <div class="footer-icons__desc">동네생활</div>
-    </div>
-    <div class="footer-icons">
-      <div class="footer-icons__img">
-        <img src="assets/location.svg" alt="" />
-      </div>
-      <div class="footer-icons__desc">내 근처</div>
-    </div>
-    <div class="footer-icons">
-      <div class="footer-icons__img">
-        <img src="assets/chat.svg" alt="" />
-      </div>
-      <div class="footer-icons__desc">채팅</div>
-    </div>
-    <div class="footer-icons">
-      <div class="footer-icons__img">
-        <img src="assets/user.svg" alt="" />
-      </div>
-      <div class="footer-icons__desc">나의 당근</div>
-    </div>
-  </div>
-</footer>
+<Footer location="home" />
 
 <div class="media-info-msg">화면 사이즈를 줄여주세요.</div>
--- src/pages/Write.svelte
+++ src/pages/Write.svelte
@@ -1,25 +1,90 @@
-<form id="write-form">
+<script>
+  import { getDatabase, ref, push } from "firebase/database";
+  import Footer from "../components/Footer.svelte";
+  import {
+    getStorage,
+    ref as refImage,
+    uploadBytes,
+    getDownloadURL,
+  } from "firebase/storage";
+
+  let title;
+  let price;
+  let description;
+  let place;
+  let files;
+
+  const storage = getStorage();
+  const db = getDatabase();
+
+  async function writeUserData(imgUrl) {
+    push(ref(db, "items/"), {
+      title,
+      price,
+      description,
+      place,
+      insertAt: new Date().getTime(),
+      imgUrl,
+    });
+    alert("글쓰기가 완료되었습니다.");
+    window.location.hash = "/";
+  }
+
+  const uploadFile = async () => {
+    const file = files[0];
+    const name = file.name;
+    const imgRef = refImage(storage, name);
+    await uploadBytes(imgRef, file); //업로드를 한다음에 그 이미지의
+    const url = await getDownloadURL(imgRef); //url 을 가져온다 url
+    return url; //이유 데이터베이스에 url을 넣어주기위해서
+  };
+
+  const handleSubmit = async () => {
+    const url = await uploadFile(); //업로드 파일에서 url을 리턴할때까지 await 기달린다.
+    writeUserData(url); //return url을 받아고 유저데이터를 업데이트
+  };
+</script>
+
+<form id="write-form" on:submit|preventDefault={handleSubmit}>
   <div>
     <label for="image">이미지</label>
-    <input type="file" id="image" name="image" />
+    <input type="file" bind:files id="image" name="image" />
   </div>
   <div>
     <label for="title">제목</label>
-    <input type="text" id="title" name="title" />
+    <input type="text" id="title" name="title" bind:value={title} />
   </div>
   <div>
     <label for="price">가격</label>
-    <input type="number" id="price" name="price" />
+    <input type="number" id="price" name="price" bind:value={price} />
   </div>
   <div>
     <label for="description">설명</label>
-    <input type="text" id="description" name="description" />
+    <input
+      type="text"
+      id="description"
+      name="description"
+      bind:value={description}
+    />
   </div>
   <div>
     <label for="place">장소</label>
-    <input type="text" id="place" name="place" />
+    <input type="text" id="place" name="place" bind:value={place} />
   </div>
   <div>
-    <button type="submit">글쓰기 완료</button>
+    <button class="write-button" type="submit">글쓰기 완료</button>
   </div>
 </form>
+
+<Footer location="write" />
+
+<style>
+  .write-button {
+    background-color: tomato;
+    margin: 10px;
+    border-radius: 10px;
+    padding: 5px 12px 5px 12px;
+    color: white;
+    cursor: pointer;
+  }
+</style>
